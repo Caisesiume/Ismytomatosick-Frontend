2d46d3931fb4cea9d188559e5c9860aa39341c52
Caisesiume <simonarvidsson91@gmail.com>
2022-12-08 16:10:04 +0100

    Fixed deploy file error

--- TomatoDiagnostics/manage.py
+++ TomatoDiagnostics/manage.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '/TomatoDiagnostics/core/settings.py')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', './core/settings.py')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
