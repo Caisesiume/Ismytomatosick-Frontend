619db7f39e11a935da6aeed6c0b3ae7beaa3b2c6
Kamila Y <gusyoska@student.gu.se>
2022-12-09 21:10:30 +0100

    add a small change to trigger vercel build

--- TomatoDiagnostics/core/urls.py
+++ TomatoDiagnostics/core/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
 
-    # ADD NEW Routes HERE
 
     # Leave `Home.Urls` as last the last line
     path("", include("apps.home.urls"))
