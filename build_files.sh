d5d737a2220aaec8c72efdae03ea9b3440a2ddb7
Kamila Y <gusyoska@student.gu.se>
2022-12-09 20:54:00 +0100

    modify the build file to avoid pip error

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 # build_files.sh
-pip install -r requirements.txt
-pip install --root-user-action=ignore requests
+# pip install -r requirements.txt
+# pip install --root-user-action=ignore requests
 python3.9 TomatoDiagnostics/manage.py runserver
